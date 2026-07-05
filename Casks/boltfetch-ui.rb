efbf64af9b5364313c165dff11448bf33b67d4b7
Sri Vinay A <30663011+SriVinayA@users.noreply.github.com>
2026-07-05 04:25:35 -0400

    Update BoltFetch UI to version 0.2.0

--- Casks/boltfetch-ui.rb
+++ Casks/boltfetch-ui.rb
@@ -1,10 +1,10 @@
 cask "boltfetch-ui" do
-  version "0.1.0"
+  version "0.2.0"
   # Replace with the sha256 hash you generated for the DMG file
-  sha256 "8bc8475b69e1327f7d057ab6a14f22fa86dd8efed019f64a4c6712712c0634d3"
+  sha256 "600f3cdc80dce925f5d1a58c635796d379bdc92b85e07b637ed94050c1f611a7"
 
   # Replace with the actual URL of the DMG file you uploaded to the GitHub Release
-  url "https://github.com/SriVinayA/BoltFetch/releases/download/v0.1.0/BoltFetch_0.1.0_aarch64.dmg"
+  url "https://github.com/SriVinayA/BoltFetch/releases/download/v0.2.0/BoltFetch_0.2.0_aarch64.dmg"
   name "BoltFetch"
   desc "A blazing-fast, multi-threaded download manager with a native UI"
   homepage "https://github.com/SriVinayA/BoltFetch"
