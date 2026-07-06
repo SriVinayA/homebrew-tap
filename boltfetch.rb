b09015737be0cb746322decb714bf3ce5c010c9e
Sri Vinay A <30663011+SriVinayA@users.noreply.github.com>
2026-07-06 00:06:07 -0400

    Update URL and version for Boltfetch to 0.2.1

--- boltfetch.rb
+++ boltfetch.rb
@@ -2,10 +2,10 @@ class Boltfetch < Formula
   desc "A blazing-fast, multi-threaded download manager"
   homepage "https://github.com/SriVinayA/BoltFetch"
   # Replace with the actual URL of the CLI binary you uploaded to the GitHub Release
-  url "https://github.com/SriVinayA/BoltFetch/releases/download/v0.2.0/boltfetch"
-  version "0.2.0"
+  url "https://github.com/SriVinayA/BoltFetch/releases/download/v0.2.1/boltfetch"
+  version "0.2.1"
   # Replace with the sha256 hash you generated for the CLI binary
-  sha256 "4d290d53afd5ae2b2014edfae64492d3d514d983e50f3ef693b1b60f8b8c6a4a" 
+  sha256 "e68d47f9598cd8442c2b6cceaeae67a85391e2a8784d4e88c6ed837bf2feb976" 
 
   def install
     bin.install "boltfetch"
