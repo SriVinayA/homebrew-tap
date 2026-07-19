e1bfa066532f9db3e094145e592f322523303ae4
SriVinayA <srivinay.appari@gmail.com>
2026-07-19 16:10:33 -0400

    Bump BoltFetch to v0.4.0

--- Casks/boltfetch-ui.rb
+++ Casks/boltfetch-ui.rb
@@ -1,10 +1,10 @@
 cask "boltfetch-ui" do
-  version "0.3.0"
+  version "0.4.0"
   # Replace with the sha256 hash you generated for the DMG file
-  sha256 "fff3d3af100b1b83267316528818fa749574e85b5bd92adca52257c2ac335870"
+  sha256 "776ee15106dd8ddde8f6e4bd1def1621cf8b86fc4e26b1ddf9db48abf5fd989b"
 
   # Replace with the actual URL of the DMG file you uploaded to the GitHub Release
-  url "https://github.com/SriVinayA/BoltFetch/releases/download/v0.3.0/BoltFetch_0.3.0_aarch64.dmg"
+  url "https://github.com/SriVinayA/BoltFetch/releases/download/v0.4.0/BoltFetch_0.4.0_aarch64.dmg"
   name "BoltFetch"
   desc "A blazing-fast, multi-threaded download manager with a native UI"
   homepage "https://github.com/SriVinayA/BoltFetch"
--- boltfetch.rb
+++ boltfetch.rb
@@ -2,10 +2,10 @@ class Boltfetch < Formula
   desc "A blazing-fast, multi-threaded download manager"
   homepage "https://github.com/SriVinayA/BoltFetch"
   # Replace with the actual URL of the CLI binary you uploaded to the GitHub Release
-  url "https://github.com/SriVinayA/BoltFetch/releases/download/v0.3.0/boltfetch"
-  version "0.3.0"
+  url "https://github.com/SriVinayA/BoltFetch/releases/download/v0.4.0/boltfetch"
+  version "0.4.0"
   # Replace with the sha256 hash you generated for the CLI binary
-  sha256 "33511cae31d2988513df0ffe0d4652dab09105cad52c1fa62ea5a70c9e5055bc"
+  sha256 "ddd254ae8412752bbc96dacf7bc3c59ee6b5a5007d49fe1a5436ef8378b02e18"
 
   def install
     bin.install "boltfetch"
